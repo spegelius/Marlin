b538b7b2ca4f4961ac8da8316b92256a824d45c9
Pekka Piippo <spegelius@gmail.com>
2017-09-28 20:31:52 +0300

    Bed size constants

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -903,8 +903,8 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS 200
-#define Y_MAX_POS 200
+#define X_MAX_POS X_BED_SIZE
+#define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 350
 
 /**
