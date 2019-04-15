2b233242f025d253705ec5e3334d0ee111a093ce
Spegelius <spegelius@gmail.com>
2019-04-15 19:44:30 +0300

    Tune movement
    - acceleration XY to 700
    - junction deviation to 0.022
    - X and Y power to 1 A
    - change minimu stepper pulse to 3, SBase seems to need that

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -931,7 +931,7 @@
 #define DIGIPOT_I2C_NUM_CHANNELS 5 // 5DPRINT: 4     AZTEEG_X3_PRO: 8     MKS SBASE: 5
 // Actual motor currents in Amps. The number of entries must match DIGIPOT_I2C_NUM_CHANNELS.
 // These correspond to the physical drivers, so be mindful if the order is changed.
-#define DIGIPOT_I2C_MOTOR_CURRENTS { 0.85, 0.85, 0.9, 1, 1 }  //  MKS SBASE: 5
+#define DIGIPOT_I2C_MOTOR_CURRENTS { 1, 1, 0.9, 1, 1 }  //  MKS SBASE: 5
 
 //===========================================================================
 //=============================Additional Features===========================
@@ -1692,6 +1692,7 @@
  * Override the default value based on the driver type set in Configuration.h.
  */
 //#define MINIMUM_STEPPER_PULSE 2
+#define MINIMUM_STEPPER_PULSE 3
 
 /**
  * Maximum stepping rate (in Hz) the stepper driver allows
@@ -1706,6 +1707,7 @@
  * Override the default value based on the driver type set in Configuration.h.
  */
 //#define MAXIMUM_STEPPER_RATE 250000
+//#define MAXIMUM_STEPPER_RATE 100000
 
 // @section temperature
 
